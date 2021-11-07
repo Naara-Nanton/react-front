c23f288fa464f19ec8e078d93b497a61509c74f8
ezequielnanton <ezequielnanton@hotmail.com>
2021-11-07 12:20:50 -0300

    correcion de nombres

--- frontend/src/pages/Alumnos/Alumnos.js
+++ frontend/src/pages/Alumnos/Alumnos.js
@@ -1,5 +1,5 @@
 
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Axios from 'axios';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,58 +7,218 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
 import {Button, Form, Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
-import {Table, TableContainer, TableHead, TableCell, TableBody, TableRow} from '@material-ui/core';
-const baseurl="https://irso-tallervi.herokuapp.com/alumnos";
 
-function Alumnos(){
-  const [data, setData]=useState([]);
+const url="https://irso-tallervi.herokuapp.com/alumnos/";
 
-  const peticionGet=async()=>{
-   await axios.get(baseurl)
-    .then(response=>{
-      //console.log(response.data);
-      setData(response.data);
-    })
-  }
-  useEffect(async()=>{
-  await peticionGet();
-}, [])
+class Alumnos extends Component {
+   
+    constructor(props){
+        super(props);
+    
+        this.state={
+          alumnos:[] ,
+          modalEliminarAlumnos: false,
+          modalInsertarAlumnos: false,
+          form:{
+            
+            id: '',
+            nombres: '',
+            apellido: '',
+            direccion: '',
+            cod_postal:'',
+            telefono:'',
+            tipoModal: ''
+          }
+          
+        }
+        this.getAlumnos = this.getAlumnos.bind(this);
+     
+      }
+    seleccionarAlumnos=(alumno)=>{
+      this.setState({
+        tipoModal: 'actualizar',
+        form: {
+          id:alumno.id,
+          nombres:alumno.nombres,
+          apellido: alumno.apellido,
+          direccion: alumno.direccion,
+          cod_postal:alumno.cod_postal,
+          telefono: alumno.telefono
+        }
+    
+      })
+    }
+    
+    modalInsertarAlumnos=()=>{
+      this.setState({modalInsertarAlumnos: !this.state.modalInsertarAlumnos});
+    }
+    
+    
+    handleChange=async e=>{
+    e.persist();
+    await this.setState({
+    form: {
+      ...this.state.form,
+      [e.target.name]: e.target.value
+    }
+    });
+    console.log(this.state.form);
+    }
+    
+    
+    componentDidMount(){
+      this.getAlumnos();
 
-return (
-  <div className="Alumnos">
-    <TableContainer>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>id</TableCell>
-            <TableCell>Nombres</TableCell>
-            <TableCell>Apellido</TableCell>
-            <TableCell>Direccion</TableCell>
-            <TableCell>Codigo Postal</TableCell>
-            <TableCell>Telefono</TableCell>
-          </TableRow>
-        </TableHead>
-
-        <TableBody>
-        {data.map(alumnos=>(
-            <TableRow key={alumnos.id}>
-              <TableCell>{alumnos.id}</TableCell>
-              <TableCell>{alumnos.nombres}</TableCell>
-              <TableCell>{alumnos.apellido}</TableCell>
-              <TableCell>{alumnos.direccion}</TableCell>
-              <TableCell>{alumnos.cod_postal}</TableCell>
-              <TableCell>{alumnos.telefono}</TableCell>
-            </TableRow>
-          ))}
-
-        </TableBody>
-
-      </Table>
-    </TableContainer>
-
-  </div>
-
-);
-}
-export default Alumnos;
-
+     
+    }
+    
+      getAlumnos=async()=>{
+        await Axios.get('https://irso-tallervi.herokuapp.com/alumnos/')
+        .then(res=>{
+          this.setState({alumnos: res.data.alumnos});
+          console.log(res.data);
+    
+        }).catch((error)=>{
+          console.log(error);
+        });
+      }
+    
+    peticionPostAlumnos=async()=>{
+      delete this.state.form.id;
+     await axios.post(url,this.state.form)
+      .then(response=>{
+        this.modalInsertarAlumnos();
+        this.getAlumnos();
+      }).catch((error)=>{
+        console.log(error);
+        
+      });
+    }
+    
+    peticionPutAlumnos=()=>{
+      axios.put(url+this.state.form.id, this.state.form).then(response=>{
+        this.modalInsertarAlumnos();
+        this.getAlumnos();
+      })
+    }
+    
+    
+    
+      peticionDeleteAlumnos=()=>{
+        axios.delete(url+this.state.form.id)
+        .then(res=>{
+        this.setState({modalEliminarAlumnos: false});
+        this.getAlumnos();
+        }).catch((error)=>{
+          console.log(error);
+        });
+    
+      }
+    
+    
+    
+      render() {
+        const{form}=this.state;
+    
+        return (
+            <div className="Alumnos">
+              
+              <button className= "btn btn-success" onClick={()=>{this.setState({form: null, tipoModal: 'insertar'});this.modalInsertarAlumnos()}}>Agregar Alumnos</button>
+              
+              
+                <table className="table">
+                    <thead className= "thead-dark">
+                        <tr>
+                            <th scope="col"><h3>ID</h3></th>
+                            <th scope="col"><h3>Nombres</h3></th>
+                            <th scope="col"><h3>Apellido</h3></th>
+                            <th scope="col"><h3> Direccion</h3></th>
+                            <th scope="col"><h3> Codigo Postal</h3></th>
+                            <th scope="col"><h3> Telefono</h3></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+    
+                        {  this.state.alumnos.map(alumno=>{
+                        return(
+                                 <tr key={alumno.id}>
+                                    <th>{alumno.id}</th>
+                                    <th>{alumno.nombres}</th>
+                                    <th >{alumno.apellidos}</th>
+                                    <th >{alumno.direccion}</th>
+                                    <th >{alumno.cod_postal}</th>
+                                    <th >{alumno.telefono}</th>   
+                                    <td>                      
+                                        <button className="btn btn-secondary"onClick= {()=>{this.seleccionarAlumnos(alumno); this.modalInsertarAlumnos()} }><FontAwesomeIcon icon={faEdit}/>  Editar</button> {"   "}                                             
+                                        <button className="btn btn-danger" onClick= {()=>{this.seleccionarAlumnos(alumno);this.setState({modalEliminarAlumnos:true})}}><FontAwesomeIcon icon={faTrashAlt}/>  Eliminar </button> {"   "}
+                                    </td> 
+                                </tr>
+                             )
+                          })}
+                    </tbody>
+                </table>
+    
+       <Modal isOpen={this.state.modalInsertarAlumnos}>
+                  <ModalHeader style={{display:'block'}}>
+                    <span style={{float:'rigth'}}>Alumnos</span>
+                    </ModalHeader>          
+                <ModalBody>
+                  <div className="form-group">
+                   
+                    <label htmlFor="id" >id</label>
+                   {/*  <input className="form-control" type= "text" name= "id" id="id" readOnly onChange={this.handleChange} value ={form?form.id:this.stat.id.length+1} /> */}
+                   <input className="form-control" type= "text" name= "id" id="id" readOnly onChange={this.handleChange} value ={form?form.id: ''} />
+                    <br  />
+                    <label htmlFor="nombres" >Nombres</label>
+                    <input className="form-control" type= "text" name= "nombres" id= "nombres"onChange={this.handleChange}value ={form?form.nombres: ' '} />
+                    <br  />
+                    <label htmlFor="apellido" >Apellido</label>
+                    <input className="form-control" type= "text" name= "apellido" id="apellido"onChange={this.handleChange}value ={form?form.apellido: ' '} />
+                    <br  />
+                    <label htmlFor="direccion" >Direccion</label>
+                    <input className="form-control" type= "text" name= "direccion" id="direccion"onChange={this.handleChange} value={form?form.direccion: ' '} />
+                
+                  <label htmlFor="cod_postal" >Codigo Postal</label>
+                    <input className="form-control" type= "text" name= "cod_postal" id="cod_postal"onChange={this.handleChange} value={form?form.cod_postal: ' '} />
+                
+                  <label htmlFor="telefono" >Telefono</label>
+                    <input className="form-control" type= "text" name= "telefono" id="telefono"onChange={this.handleChange} value={form?form.telefono: ' '} />
+                  </div>
+    
+                </ModalBody>
+                <ModalFooter>
+                  {this.state.tipoModal=='insertar'?
+                  <button className="btn btn-success"onClick={()=>this.peticionPostAlumnos()}>
+                     Insertar
+                     
+                     </button>:<button className="btn btn-success"onClick={()=>this.peticionPutAlumnos()}> 
+                     Actualizar
+                     </button>
+                        }
+                  <button className="btn btn-danger" onClick={()=>this.modalInsertarAlumnos()} >Cerrar </button>
+    
+                </ModalFooter>
+    
+                </Modal>
+                  
+    
+              
+    
+                <Modal isOpen={this.state.modalEliminarAlumnos}>
+                  <ModalBody>
+                    Estas seguro que quieres eliminar el registro?  { form && form.id}
+    
+                  </ModalBody>
+                  <ModalFooter>
+                    <button className="btn btn-danger" onClick={()=>this.peticionDeleteAlumnos()}>Si</button>
+                    <button className="btn btn-secondary"onClick={()=>this.setState({modalEliminarAlumnos: false})} >No</button>
+                  </ModalFooter>
+                </Modal>
+            </div>
+    
+        );
+      }
+    }
+    
+    export default Alumnos;
+    
